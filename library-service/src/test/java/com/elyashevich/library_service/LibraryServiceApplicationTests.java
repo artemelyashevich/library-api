fdf4c58673d6bddae60838e9d4448269cdfe1a18
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-08-27 14:04:46 +0300

    Add: services

---
+++ library-service/src/test/java/com/elyashevich/library_service/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyashevich.library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
