059c4d00d09413aab1424e526835bf57c739033b
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-08-29 21:24:06 +0300

    Code refactoring

--- authentication-service/src/main/java/com/elyashevich/authentication/api/dto/AuthResponse.java
+++ authentication-service/src/main/java/com/elyashevich/authentication/api/dto/AuthResponse.java
@@ -1,9 +1,7 @@
 package com.elyashevich.authentication.api.dto;
 
-import jakarta.validation.constraints.NotNull;
 
 public record AuthResponse(
-        @NotNull(message = "Token must be not null.")
         String token
 ) {
 }
