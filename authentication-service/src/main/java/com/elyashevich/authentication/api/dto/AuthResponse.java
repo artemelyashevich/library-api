ea95faa285dc45ecfb34e617a43f271877e111d4
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-08-29 21:21:25 +0300

    Add: auth service

--- authentication-service/src/main/java/com/elyashevich/authentication/api/dto/AuthResponse.java
+++ authentication-service/src/main/java/com/elyashevich/authentication/api/dto/AuthResponse.java
@@ -1,6 +1,9 @@
 package com.elyashevich.authentication.api.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record AuthResponse(
+        @NotNull(message = "Token must be not null.")
         String token
 ) {
 }
